ba63ede7102ee05ab4728184a6a02d2fc0067e83
savvas-paragkamian <s.paragkamian@gmail.com>
2025-04-01 18:18:06 +0300

    initialise script for multiple assemblies

---
+++ scripts/biosolutions_assembly.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -l
+
+# Name: biosolutions_assembly.sh
+# Purpuse: perform bacterial assembly to multiple isolates
+# Author: Savvas Paragkamian
+# Date: 01/04/2025
+
+## example of running the script
+## conda activate perfect_assembly
+
+
+#cd /media/sarlab/DATA/
+#md5sum -c BGI_result/md5.txt 
+# if all is ok proceed to uncompress
+
+cd /media/sarlab/DATA/BGI_result
+
